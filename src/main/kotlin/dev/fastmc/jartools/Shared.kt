6a7df947de9f98be610f3f6c24914672e5fa2aa7
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-27 01:37:29 -0500

    Maven cache

---
+++ src/main/kotlin/dev/fastmc/jartools/Shared.kt
@@ -0,0 +1,7 @@
+package dev.fastmc.jartools
+
+import java.io.File
+
+object Shared {
+    lateinit var globalCacheDir : File
+}
---
+++ src/main/kotlin/dev/fastmc/jartools/util/GlobalMavenCache.kt
@@ -0,0 +1,39 @@
+package dev.fastmc.jartools.util
+
+import dev.fastmc.jartools.Shared
+import kotlinx.coroutines.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.net.URL
+
+object GlobalMavenCache {
+    private val scope = CoroutineScope(Dispatchers.Default)
+    private val cacheDir by lazy { File(Shared.globalCacheDir, "maven") }
+
+    private val mavenRegex = "maven.*?/(.*)".toRegex()
+
+    fun getMaven(url: String): Deferred<File?> {
+        check(url.startsWith("http")) { "URL must start with http" }
+        check(url.contains(mavenRegex)) { "URL must contain maven" }
+        val localPath = mavenRegex.find(url)!!.groupValues[1]
+
+        val local = File(cacheDir, localPath)
+        if (local.exists()) {
+            return CompletableDeferred(local)
+        }
+        return scope.async(Dispatchers.IO) {
+            try {
+                URL(url).openStream().use { input ->
+                    local.parentFile.mkdirs()
+                    local.createNewFile()
+                    local.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+                local
+            } catch (e: FileNotFoundException) {
+                null
+            }
+        }
+    }
+}
