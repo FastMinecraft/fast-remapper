b0619d761ca4842448c131c769d6fe0fe67dfbde
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-27 17:19:42 -0500

    Use Mutex in GlobalMavenCache

--- src/main/kotlin/dev/fastmc/remapper/util/GlobalMavenCache.kt
+++ src/main/kotlin/dev/fastmc/remapper/util/GlobalMavenCache.kt
@@ -1,38 +1,42 @@
 package dev.fastmc.remapper.util
 
 import dev.fastmc.remapper.Shared
+import it.unimi.dsi.fastutil.objects.Object2ObjectMaps
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.io.File
-import java.io.FileNotFoundException
 import java.net.URL
 
 object GlobalMavenCache {
     private val scope = CoroutineScope(Dispatchers.Default)
     private val cacheDir get() = Shared.mavenCacheDir
+    private val locks = Object2ObjectMaps.synchronize(Object2ObjectOpenHashMap<String, Mutex>())
 
     private val mavenRegex = "maven.*?/(.*)".toRegex()
 
-    fun getMaven(url: String): Deferred<File?> {
+    suspend fun getMaven(url: String): Deferred<File?> {
         check(url.startsWith("http")) { "URL must start with http" }
         check(url.contains(mavenRegex)) { "URL must contain maven" }
         val localPath = mavenRegex.find(url)!!.groupValues[1]
 
         val local = File(cacheDir, localPath)
-        if (local.exists()) {
-            return CompletableDeferred(local)
-        }
-        return scope.async(Dispatchers.IO) {
-            try {
-                URL(url).openStream().use { input ->
-                    local.parentFile.mkdirs()
-                    local.createNewFile()
-                    local.outputStream().use { output ->
-                        input.copyTo(output)
+        val mutex = locks.computeIfAbsent(localPath) { _: String -> Mutex() }
+        return mutex.withLock {
+            if (local.exists()) {
+                CompletableDeferred(local)
+            } else {
+                scope.async(Dispatchers.IO) {
+                    URL(url).openStream().use { input ->
+                        local.parentFile.mkdirs()
+                        local.createNewFile()
+                        local.outputStream().use { output ->
+                            input.copyTo(output)
+                        }
                     }
+                    local
                 }
-                local
-            } catch (e: FileNotFoundException) {
-                null
             }
         }
     }
